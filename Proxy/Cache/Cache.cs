9d657653ee50bb587ad059ecbbda122f7a8bb4b6
Lucas RAKOTOMALALA <lucas.rakotomalala@yahoo.fr>
2021-04-04 23:09:50 +0200

    Mise en place du cache

---
+++ Proxy/Cache/Cache.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Caching;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Proxy.Cache
+{
+    public class Cache<T> where T : new()
+    {
+        private ObjectCache cache;
+        private DateTimeOffset dt_default { get; set; }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public Cache()
+        {
+            cache = MemoryCache.Default;
+            dt_default = ObjectCache.InfiniteAbsoluteExpiration;
+        }
+
+        public T Get(string CacheItem)
+        {
+            return Get(CacheItem, dt_default);
+        }
+
+        public T Get(string CacheItem, double dt_seconds)
+        {
+            return Get(CacheItem, DateTimeOffset.Now.AddSeconds(dt_seconds));
+        }
+
+        public T Get(string CacheItem, DateTimeOffset dt)
+        {
+            if (cache.Get(CacheItem) == null)
+            {
+                CacheItemPolicy cacheItemPolicy = new CacheItemPolicy
+                {
+                    AbsoluteExpiration = dt
+                };
+                cache.Add(CacheItem, CallAPI(CacheItem).Result, cacheItemPolicy);
+            }
+            return (T) cache.Get(CacheItem);
+        }
+
+        private static async Task<T> CallAPI(string request)
+        {
+            System.Diagnostics.Debug.WriteLine("CallAPI");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(request);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<T>(responseBody);
+            }
+            catch (HttpRequestException)
+            {
+                return new T();
+            }
+        }
+    }
+}
--- Proxy/Services/JCDecaux.cs
+++ Proxy/Services/JCDecaux.cs
@@ -1,41 +1,22 @@
-﻿using Proxy.Models;
-using System;
+﻿using Proxy.Cache;
+using Proxy.Models;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Proxy
 {
     public class JCDecaux : IJCDecaux
     {
-        static readonly HttpClient client = new HttpClient();
+        private static readonly string URL = "https://api.jcdecaux.com/vls/v2/";
+        private static readonly string DATA = "stations";
+        private static readonly string API_KEY = "ff987c28b1313700e2c97651cec164bd6cb4ed76";
 
-        static readonly string URL = "https://api.jcdecaux.com/vls/v2/";
-        static readonly string DATA = "stations";
-        static readonly string API_KEY = "ff987c28b1313700e2c97651cec164bd6cb4ed76";
+        private Cache<List<Station>> stations = new Cache<List<Station>>();
+        private readonly double EXPIRATION_TIME = 30;
 
         List<Station> IJCDecaux.GetAllStationsFromCity(string city)
         {
-            return GetAllStationsFromCity(city).Result;
+            return stations.Get(URL + DATA + "?contract=" + city + "&apiKey=" + API_KEY, EXPIRATION_TIME);
         }
 
-        private static async Task<List<Station>> GetAllStationsFromCity(string city)
-        {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(URL + DATA + "?contract=" + city + "&apiKey=" + API_KEY);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-                return JsonSerializer.Deserialize<List<Station>>(responseBody);
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                return new List<Station>();
-            }
-        }
     }
 }
