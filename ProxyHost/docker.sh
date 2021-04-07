f6be0c6a069e7d9207cfa1165a4845b37c751d0f
Lucas RAKOTOMALALA <lucas.rakotomalala@yahoo.fr>
2021-04-07 15:52:49 +0200

    Ajout de commentaires dans l'exécutable

--- ProxyHost/docker.sh
+++ ProxyHost/docker.sh
@@ -1,5 +1,11 @@
+# Pull the image first, the one used in ProxyHost Dockerfile
+docker pull mcr.microsoft.com/dotnet/framework/wcf:4.8
+
+# Create the image
 docker build -t wcfhost:lastest .
 
+# Run the container
 docker run -itd --name ProxyHost wcfhost
 
-docker inspect -f="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" ProxyHost
+# Get IP Address
+docker inspect -f="{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" ProxyHost 
